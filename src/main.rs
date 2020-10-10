9a8b417a2fd4be2cd1148fe4b4cda7076e791908
Krouton <krouton@hatena.ne.jp>
2020-10-10 15:39:14 +0900

    Use clap and add some options.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::fs::{self, File};
 use std::io::{BufReader, Read, Write};
 use std::path::Path;
 
+use clap::{App, Arg};
+
 const DOT_GIT: &'static str = "./workdir/.git";
 
 fn git_init() -> Option<()> {
@@ -31,59 +33,57 @@ fn git_init() -> Option<()> {
     Some(())
 }
 
-fn main() {
-    let args: Vec<_> = env::args().skip(1).collect();
-    let sub = args.get(0).unwrap();
-    if sub == &"init".to_string() {
-        dbg!(git_init());
-    } else if sub == &"cat-file".to_string() {
-        // echo 'test content' | git hash-object -w --stdin
-        // blob 13\x00test content\n
-        // d670460b4b4aece5915caf5c68d12f560a9fe3e4
-        // git cat-file -p d670460b4b4aece5915caf5c68d12f560a9fe3e4
-        // test content
-        // blob 13\u{0}test content\n
-        // let hash = "d670460b4b4aece5915caf5c68d12f560a9fe3e4";
+fn cat_file(hash: &str) {
+    // echo 'test content' | git hash-object -w --stdin
+    // blob 13\x00test content\n
+    // d670460b4b4aece5915caf5c68d12f560a9fe3e4
+    // git cat-file -p d670460b4b4aece5915caf5c68d12f560a9fe3e4
+    // test content
+    // blob 13\u{0}test content\n
+    // let hash = "d670460b4b4aece5915caf5c68d12f560a9fe3e4";
 
-        let hash = args.get(1).unwrap();
+    // let hash = args.get(1).unwrap();
 
-        let dir = &hash[0..2];
-        let file = &hash[2..];
-        let path_str = format!("{}/objects/{}/{}", DOT_GIT, dir, file);
-        let path = Path::new(&path_str);
-        let file = File::open(path).expect(&format!("cannoot find the path: {}", path.display()));
-        let f = BufReader::new(file);
-        let mut decoder = Decoder::new(f).unwrap();
-        let mut buf = String::new();
-        decoder
-            .read_to_string(&mut buf)
-            .expect("i don't know what to do.");
-        dbg!(buf);
-    } else if sub == &"hash-object".to_string() {
-        // ❯ echo 'test content' | git hash-object --stdin
-        // d670460b4b4aece5915caf5c68d12f560a9fe3e4
-        // blob 13\u{0}test content\n
+    let dir = &hash[0..2];
+    let file = &hash[2..];
+    let path_str = format!("{}/objects/{}/{}", DOT_GIT, dir, file);
+    let path = Path::new(&path_str);
+    let file = File::open(path).expect(&format!("cannoot find the path: {}", path.display()));
+    let f = BufReader::new(file);
+    let mut decoder = Decoder::new(f).unwrap();
+    let mut buf = String::new();
+    decoder
+        .read_to_string(&mut buf)
+        .expect("i don't know what to do.");
+    dbg!(buf);
+}
 
-        let content = args.get(1).unwrap();
-        // for debug.
-        let content = format!("{}\n", content);
-        let length = content.as_bytes().len();
-        let will_be_blobed = format!("blob {}{}{}", length, '\x00', content);
-        dbg!(&will_be_blobed);
+fn hash_object(content: &str, should_write: bool) {
+    // ❯ echo 'test content' | git hash-object --stdin
+    // d670460b4b4aece5915caf5c68d12f560a9fe3e4
+    // blob 13\u{0}test content\n
 
-        let mut hasher = Sha1::new();
-        hasher.input_str(&will_be_blobed);
-        let hex = hasher.result_str();
-        dbg!(&hex);
+    // for debug.
+    let content = format!("{}\n", content);
 
-        // Encoding
-        let mut encoder = Encoder::new(Vec::new()).unwrap();
-        let mut will_be_blobed = will_be_blobed.as_bytes();
-        std::io::copy(&mut will_be_blobed, &mut encoder).unwrap();
+    let length = content.as_bytes().len();
+    let will_be_blobed = format!("blob {}{}{}", length, '\x00', content);
+    dbg!(&will_be_blobed);
 
-        let res = encoder.finish();
-        let mut encoded = res.unwrap().0.clone();
+    let mut hasher = Sha1::new();
+    hasher.input_str(&will_be_blobed);
+    let hex = hasher.result_str();
+    println!("{}", &hex);
 
+    // Encoding
+    let mut encoder = Encoder::new(Vec::new()).unwrap();
+    let mut will_be_blobed = will_be_blobed.as_bytes();
+    std::io::copy(&mut will_be_blobed, &mut encoder).unwrap();
+
+    let res = encoder.finish();
+    let mut encoded = res.unwrap().0.clone();
+
+    if should_write {
         let dir = &hex[0..2];
         let file = &hex[2..];
         let create_dir = format!("{}/objects/{}", DOT_GIT, dir);
@@ -91,7 +91,53 @@ fn main() {
         let create_file = format!("{}/{}", create_dir, file);
         let mut file = File::create(create_file).expect("what?");
         dbg!(file.write_all(&mut encoded));
-    } else {
-        unimplemented!();
     };
+}
+
+fn main() {
+    let matches = App::new("Krouton's own git")
+        .version("0.1")
+        .author("Krout0n <me@tokinia.me>")
+        .subcommand(App::new("init").about("perhaps it works as you expect"))
+        .subcommand(
+            App::new("cat-file")
+                .about("pretty-print object's content")
+                .arg(Arg::new("hash")),
+        )
+        .subcommand(
+            App::new("hash-object")
+                .about("Compute object ID and optionally creates a blob from a file")
+                .arg(Arg::new("content"))
+                .arg(
+                    Arg::new("write")
+                        .short('w')
+                        .long("write")
+                        .about("Create file"),
+                ),
+        )
+        .get_matches();
+    let subcmds = matches.subcommand().unwrap();
+    match subcmds.0 {
+        "init" => {
+            dbg!(git_init());
+        }
+        "cat-file" => {
+            let args = subcmds.1;
+            if let Some(hash) = args.value_of("hash") {
+                cat_file(hash);
+            } else {
+                todo!();
+            }
+        }
+        "hash-object" => {
+            let args = subcmds.1;
+            if let Some(content) = args.value_of("content") {
+                hash_object(content, args.is_present("write"));
+            } else {
+                todo!();
+            }
+        }
+
+        _ => unimplemented!(),
+    }
 }
